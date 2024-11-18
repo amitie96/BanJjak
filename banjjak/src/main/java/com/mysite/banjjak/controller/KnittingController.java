84fa788595c148a81b58587e3145af4dee9ec547
yerangg <yerang_030@naver.com>
2024-11-18 22:13:45 +0900

    11/18 예랑 knitting 사진 추가(하는중)

--- banjjak/src/main/java/com/mysite/banjjak/controller/KnittingController.java
+++ banjjak/src/main/java/com/mysite/banjjak/controller/KnittingController.java
@@ -1,8 +1,16 @@
 package com.mysite.banjjak.controller;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
@@ -18,6 +26,23 @@ public class KnittingController {
 	public String write() {
 		return "knitting/write";
 	}
+	
+//	하다 말았음 예랑이가 19일에 할 예정	
+//	@PostMapping("/write")
+//	String upload(Knitting knitting, MultipartFile uploadFile) {
+//		if(!uploadFile.isEmpty()) {
+//			String filename = uploadFile.getOriginalFilename();
+//			
+//			try {
+//				uploadFile.transferTo(new File("d:/upload/knitting/" + filename));
+//				knitting.setFilename(filename);
+//			} catch (Exception e) {
+//				return "redirect:/knitting/write";
+//			}
+//		}
+//		return "redirect:/knitting/list";	
+//	}
+	
 }
 
 
---
+++ banjjak/src/main/java/com/mysite/banjjak/model/Knitting.java
@@ -0,0 +1,65 @@
+package com.mysite.banjjak.model;
+
+public class Knitting {
+	
+	private int knitId;
+	private String knitTitle;
+	private String knitContent;
+	private String knitDate;
+	private String knitRate;
+	private String userId;
+	
+	private String filename;
+	private String uuid;	
+	
+	public int getKnitId() {
+		return knitId;
+	}
+	public void setKnitId(int knitId) {
+		this.knitId = knitId;
+	}
+	public String getKnitTitle() {
+		return knitTitle;
+	}
+	public void setKnitTitle(String knitTitle) {
+		this.knitTitle = knitTitle;
+	}
+	public String getKnitContent() {
+		return knitContent;
+	}
+	public void setKnitContent(String knitContent) {
+		this.knitContent = knitContent;
+	}
+	public String getKnitDate() {
+		return knitDate;
+	}
+	public void setKnitDate(String knitDate) {
+		this.knitDate = knitDate;
+	}
+	public String getKnitRate() {
+		return knitRate;
+	}
+	public void setKnitRate(String knitRate) {
+		this.knitRate = knitRate;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	
+}
